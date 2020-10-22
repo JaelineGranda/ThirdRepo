8d505a3f39316fbfc6c5b1127e8e471a759c33e5
Jaeline Granda <granda.j@northeastern.edu>
2020-10-22 00:06:58 -0400

    Added code comments

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,26 +1,24 @@
-const contentsPath = 'data/task.JSON';
+const contentsPath = 'data/task.JSON'; //JSON file
 
 const populate = (contents) => {
     const todoContainer = document.getElementById('todo-container');
-    const taskTable = document.createElement('table');
+    const taskTable = document.createElement('table'); // create table
     taskTable.setAttribute('id', 'taskTable');
     let row, col;
 
-
     contents.forEach(task => {
         row = document.createElement('tr');
-        let x = document.createElement("INPUT");
+        let x = document.createElement("INPUT"); // create checkbox for user interaction
         x.setAttribute("type", "checkbox");
-        row.appendChild(x);
+        row.appendChild(x); // add checkbox to row
         col = document.createElement('td');
         col.innerHTML = task.title;
         row.appendChild(col);
         col.addEventListener('click', (evt) =>{ //show details when clicked
             const target = evt.target;
-            let modalBtn = document.getElementById("modal-btn")
             let modal = document.querySelector(".modal")
             let modalcontent = document.getElementById('detailView');
-            modalcontent.innerHTML = "";
+            modalcontent.innerHTML = ""; // removes previous task details
             modal.style.display = "block";
             p = document.createElement('p');
             p.innerHTML = task.title + ":"; //shows task description
@@ -34,22 +32,22 @@ const populate = (contents) => {
             p = document.createElement('p');
             p.innerHTML = task.time; //shows task description
             modalcontent.appendChild(p);
-            window.onclick = function(event) {
+            window.onclick = function(event) { // to exit modal pop-up click outside
                 if (event.target == modal) {
-                  modal.style.display = "none";
+                  modal.style.display = "none"; //remove modal from screen
                 }
             }
         });
-        taskTable.appendChild(row);
+        taskTable.appendChild(row); 
     });
-    todoContainer.appendChild(taskTable);
+    todoContainer.appendChild(taskTable); // adds table to div
 }
 
 //Click add button to display form for adding task
 const addButton = document.getElementById('task-add');
 addButton.addEventListener('click', () =>{
     let formdisplay = document.getElementById("addform");
-    formdisplay.style.display = "block";
+    formdisplay.style.display = "block"; // when add button is clicked, form is displayed
 })
 
 
@@ -58,22 +56,21 @@ const submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', () =>{ //Call when submit button is clicked
     let formdisplay = document.getElementById("addform");
     formdisplay.style.display = "none"; //Remove form after item is submitted
-    let table = document.getElementById("taskTable"); //refer to created table
     row = document.createElement('tr');
-    let x = document.createElement("INPUT");
+    let x = document.createElement("INPUT"); //creates checkbox for user interaction
     x.setAttribute("type", "checkbox");
     row.appendChild(x);
     col = document.createElement('td');
-    col.innerHTML = document.getElementById("newtitle").value;
+    col.innerHTML = document.getElementById("newtitle").value; //adds user input value to row
     row.appendChild(col);
     col = document.createElement('td');
-    col.innerHTML = document.getElementById("newdescription").value;
+    col.innerHTML = document.getElementById("newdescription").value; //adds input value to row
     row.appendChild(col);
     col = document.createElement('td');
-    col.innerHTML = document.getElementById("newduedate").value;
+    col.innerHTML = document.getElementById("newduedate").value; //adds user input value to row
     row.appendChild(col);
-        col = document.createElement('td');
-    col.innerHTML = document.getElementById("newtime").value;
+    col = document.createElement('td');
+    col.innerHTML = document.getElementById("newtime").value; //adds user input value to row
     row.appendChild(col);
     taskTable.appendChild(row);
 })
